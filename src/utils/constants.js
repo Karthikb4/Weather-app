b6cac22908b74b3d9e13689f9f877e809d98ecc9
Karthik <bankapallikarthik03@gmail.com>
2025-04-04 18:05:09 +0530

    minor twiks

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 const API_KEY = "876906726d7a8d6c42041bcff336a5b3";
 const BASE_URL = "https://api.openweathermap.org/data/2.5/";
-const SUGGESTIONS_BASE_URL="https://api.openweathermap.org/geo/1.0/reverse?";
+const SUGGESTIONS_BASE_URL="https://api.openweathermap.org/geo/1.0/direct?";
 // `https://api.openweathermap.org/geo/1.0/direct?q=${searchText}&limit=5&appid=YOUR_API_KEY`
 export {API_KEY,BASE_URL,SUGGESTIONS_BASE_URL}
